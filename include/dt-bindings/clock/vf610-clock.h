0c3bba168098855b887c611823cce475b8ccf6ce
Lukasz Majewski <lukma@denx.de>
2025-02-19 13:48:54 +0100

    dt-bindings: clock: vf610: Add definitions for MTIP L2 switch
    This patch adds VF610_CLK_ESW and VF610_CLK_ESW_MAC_TABLE macros
    definitions for L2 switch.
    
    Those definitions describe clocks for MoreThanIP switch IP block;
    the switch itself and the MAC address lookup table.
    
    Signed-off-by: Lukasz Majewski <lukma@denx.de>

--- include/dt-bindings/clock/vf610-clock.h
+++ include/dt-bindings/clock/vf610-clock.h
@@ -197,6 +197,8 @@
 #define VF610_CLK_TCON1			188
 #define VF610_CLK_CAAM			189
 #define VF610_CLK_CRC			190
-#define VF610_CLK_END			191
+#define VF610_CLK_ESW			191
+#define VF610_CLK_ESW_MAC_TAB		192
+#define VF610_CLK_END			193
 
 #endif /* __DT_BINDINGS_CLOCK_VF610_H */

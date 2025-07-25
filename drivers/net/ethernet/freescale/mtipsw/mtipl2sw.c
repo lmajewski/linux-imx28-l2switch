228333689efe6fa6522b183fd6ab9e141bb91994
Lukasz Majewski <lukasz.majewski@mailbox.org>
2025-07-25 12:35:27 +0200

    net: mtip: Support for adding and removing fdb entries in MAC table [SWITCHDEV]
    Support for adding and removing entries into the MTIP MAC address table.
    
    Signed-off-by: Lukasz Majewski <lukasz.majewski@mailbox.org>

--- drivers/net/ethernet/freescale/mtipsw/mtipl2sw.c
+++ drivers/net/ethernet/freescale/mtipsw/mtipl2sw.c
@@ -2153,11 +2153,15 @@ static int mtip_sw_probe(struct platform_device *pdev)
 	if (ret)
 		goto disable_clk_bulk;
 
+	ret = mtip_switchdev_register_notifiers(fep);
+	if (ret)
+		goto unregister_bridge_notifiers;
+
 	ret = mtip_switch_dma_init(fep);
 	if (ret) {
 		dev_err(&pdev->dev, "%s: ethernet switch init fail (%d)!\n",
 			__func__, ret);
-		goto unregister_notifiers;
+		goto unregister_switchdev_notifiers;
 	}
 
 	ret = mtip_mii_init(fep, pdev);
@@ -2188,7 +2192,9 @@ static int mtip_sw_probe(struct platform_device *pdev)
 			  fep->bd_dma);
 	fep->rx_bd_base = NULL;
 	fep->tx_bd_base = NULL;
- unregister_notifiers:
+ unregister_switchdev_notifiers:
+	mtip_switchdev_unregister_notifiers(fep);
+ unregister_bridge_notifiers:
 	mtip_bridge_unregister_notifiers(fep);
  disable_clk_bulk:
         clk_bulk_disable_unprepare(fep->clk_num, fep->clks);
@@ -2200,6 +2206,7 @@ static void mtip_sw_remove(struct platform_device *pdev)
 {
 	struct switch_enet_private *fep = platform_get_drvdata(pdev);
 
+	mtip_switchdev_unregister_notifiers(fep);
 	mtip_bridge_unregister_notifiers(fep);
 
 	mtip_ndev_cleanup(fep);
---
+++ drivers/net/ethernet/freescale/mtipsw/mtipl2sw_switchdev.c
@@ -0,0 +1,165 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ *  L2 switch Controller driver for MTIP block - switchdev
+ *
+ *  Copyright (C) 2025 DENX Software Engineering GmbH
+ *  Lukasz Majewski <lukma@denx.de>
+ */
+
+#include <linux/etherdevice.h>
+#include <linux/if_bridge.h>
+#include <linux/netdevice.h>
+#include <linux/workqueue.h>
+#include <linux/platform_device.h>
+#include <net/switchdev.h>
+
+#include "mtipl2sw.h"
+
+struct mtip_switchdev_event_work {
+	struct work_struct work;
+	struct switchdev_notifier_fdb_info fdb_info;
+	struct mtip_ndev_priv *priv;
+	unsigned long event;
+};
+
+static void mtip_fdb_offload_notify(struct net_device *ndev,
+				    struct switchdev_notifier_fdb_info *rcv)
+{
+	struct switchdev_notifier_fdb_info info = {};
+
+	info.addr = rcv->addr;
+	info.vid = rcv->vid;
+	info.offloaded = true;
+	call_switchdev_notifiers(SWITCHDEV_FDB_OFFLOADED,
+				 ndev, &info.info, NULL);
+}
+
+static void mtip_switchdev_event_work(struct work_struct *work)
+{
+	struct mtip_switchdev_event_work *switchdev_work =
+		container_of(work, struct mtip_switchdev_event_work, work);
+	struct mtip_ndev_priv *priv = switchdev_work->priv;
+	struct switch_enet_private *fep = priv->fep;
+	struct switchdev_notifier_fdb_info *fdb;
+	int ret, port = priv->portnum;
+
+	rtnl_lock();
+	switch (switchdev_work->event) {
+	case SWITCHDEV_FDB_ADD_TO_DEVICE:
+		fdb = &switchdev_work->fdb_info;
+
+		dev_dbg(&fep->pdev->dev,
+		        "mtip_fdb_add: MACID = %pM vid = %u flags = %u %u -- port %d\n",
+			fdb->addr, fdb->vid, fdb->added_by_user,
+			fdb->offloaded, port);
+
+		if (!fdb->added_by_user || fdb->is_local)
+			break;
+		if (ether_addr_equal(&fep->mac[port - 1][0], (u8 *)fdb->addr))
+			port = SWITCH_HOST_PORT_NUM;
+
+		ret = mtip_set_static_table_entry((u8 *)fdb->addr, port, fep);
+		if (ret)
+			dev_err(&fep->pdev->dev,
+			        "mtip_fdb_add: Cannot write entry MACID = %pM port %d\n",
+			        fdb->addr, port);
+
+		mtip_fdb_offload_notify(priv->dev, fdb);
+		break;
+	case SWITCHDEV_FDB_DEL_TO_DEVICE:
+		fdb = &switchdev_work->fdb_info;
+
+		dev_dbg(&fep->pdev->dev,
+		        "mtip_fdb_del: MACID = %pM vid = %u flags = %u %u -- port %d\n",
+			fdb->addr, fdb->vid, fdb->added_by_user,
+			fdb->offloaded, port);
+
+		if (!fdb->added_by_user || fdb->is_local)
+			break;
+		if (ether_addr_equal(&fep->mac[port - 1][0], (u8 *)fdb->addr))
+			port = SWITCH_HOST_PORT_NUM;
+
+		ret = mtip_clear_static_table_entry((u8 *)fdb->addr, port, fep);
+		if (ret)
+			dev_err(&fep->pdev->dev,
+			        "mtip_fdb_add: Cannot clear entry MACID = %pM port %d\n",
+			        fdb->addr, port);
+		break;
+	default:
+		break;
+	}
+	rtnl_unlock();
+
+	kfree(switchdev_work->fdb_info.addr);
+	kfree(switchdev_work);
+	dev_put(priv->dev);
+}
+
+/* called under rcu_read_lock() */
+static int mtip_switchdev_event(struct notifier_block *unused,
+				unsigned long event, void *ptr)
+{
+	struct net_device *ndev = switchdev_notifier_info_to_dev(ptr);
+	struct switchdev_notifier_fdb_info *fdb_info = ptr;
+	struct mtip_switchdev_event_work *switchdev_work;
+	struct mtip_ndev_priv *priv = netdev_priv(ndev);
+
+	if (!mtip_is_switch_netdev_port(ndev))
+		return NOTIFY_DONE;
+
+	switchdev_work = kzalloc(sizeof(*switchdev_work), GFP_ATOMIC);
+	if (WARN_ON(!switchdev_work))
+		return NOTIFY_BAD;
+
+	INIT_WORK(&switchdev_work->work, mtip_switchdev_event_work);
+	switchdev_work->priv = priv;
+	switchdev_work->event = event;
+
+	switch (event) {
+	case SWITCHDEV_FDB_ADD_TO_DEVICE:
+	case SWITCHDEV_FDB_DEL_TO_DEVICE:
+		memcpy(&switchdev_work->fdb_info, ptr,
+		       sizeof(switchdev_work->fdb_info));
+		switchdev_work->fdb_info.addr = kzalloc(ETH_ALEN, GFP_ATOMIC);
+		if (!switchdev_work->fdb_info.addr)
+			goto err_addr_alloc;
+		ether_addr_copy((u8 *)switchdev_work->fdb_info.addr,
+				fdb_info->addr);
+		dev_hold(ndev);
+		break;
+	default:
+		kfree(switchdev_work);
+		return NOTIFY_DONE;
+	}
+
+	queue_work(system_long_wq, &switchdev_work->work);
+
+	return NOTIFY_DONE;
+
+err_addr_alloc:
+	kfree(switchdev_work);
+	return NOTIFY_BAD;
+}
+
+static struct notifier_block mtip_switchdev_notifier = {
+	.notifier_call = mtip_switchdev_event,
+};
+
+int mtip_switchdev_register_notifiers(struct switch_enet_private *fep)
+{
+	int ret = 0;
+
+	ret = register_switchdev_notifier(&mtip_switchdev_notifier);
+	if (ret) {
+		dev_err(&fep->pdev->dev,
+		        "register switchdev notifier fail ret:%d\n", ret);
+		return ret;
+	}
+
+	return 0;
+}
+
+void mtip_switchdev_unregister_notifiers(struct switch_enet_private *fep)
+{
+	unregister_switchdev_notifier(&mtip_switchdev_notifier);
+}
